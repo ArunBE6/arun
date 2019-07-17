4ef9d93a1a8b7357f88f221b0139c79872bc68d4
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-17 16:02:19 +0530

    Create player_set1_5.py

---
+++ player_set1_5.py
@@ -0,0 +1,6 @@
+people=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+quit=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+rang=str(input())
+for i in range(20):
+    if(rang==people[i]):
+        print(quit[i])
