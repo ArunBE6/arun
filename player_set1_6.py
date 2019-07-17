db940b0e84f0cbbe1168e7200b13d84ad374c4a8
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-17 16:05:29 +0530

    Create player_set1_6.py

---
+++ player_set1_6.py
@@ -0,0 +1,8 @@
+lenth,var=map(str,input().splenthit())
+for i in range(len(lenth)):
+    if(lenth.count(lenth[i])==var.count(var[i])):
+        print("yes")
+        break
+    else:
+        print("no")
+        break
