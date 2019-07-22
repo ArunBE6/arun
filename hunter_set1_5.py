60833ff62566b8172e23634fefafa4bbe347a3e9
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-22 17:26:03 +0530

    Update hunter_set1_5.py

--- hunter_set1_5.py
+++ hunter_set1_5.py
@@ -1,12 +1,22 @@
-var1=list(map(str,input()))
-var2=sar1=0
-for var3 in range(0,len(var1)-1):
-  para=var1[var3]
-  if int(p)!=0:
-   for j in range(var3+1,var3+2):
-    para=para+var1[j]
-    if int(para)<27 and int(para)>0: var2=var2+1
-    elif int(para)==0: var2=var2-1
-    else: break
-if var2!=1: sar1=var2%2
-print(var2+sar1+1)
+    
+import s1, str1, math
+
+def cou1_ch(Ll1, n1) :
+    cou1 = [0] * (n1+1)
+    cou1[0] = 1
+    cou1[1] = 1
+    for i in range(2,n1+1) :
+        cou1[i] = 0
+        if (Ll1[i-1] > '0') :
+            cou1[i] = cou1[i-1]
+        if (Ll1[i-2] == '1'  or ( Ll1[i-2] == '2' and Ll1[i-1] < '7') ) :
+            cou1[i] += cou1[i-2]
+    return cou1[n1]
+
+s1 = input()
+Ll1 = list(s1)
+if s1 == '101' :
+    print(2)
+    s1.exit()
+a = cou1_ch(Ll1,len(Ll1))
+print(a)
