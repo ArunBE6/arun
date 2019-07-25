76e49d0df3443e57b2e4d10811c5ee75dc7aa861
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-25 11:17:06 +0530

    Create hunter_set1_10.py

---
+++ hunter_set1_10.py
@@ -0,0 +1,14 @@
+var1=input()
+var1=var1.split()
+var2=var1[0]
+var3=var1[1]
+sum1=0
+n=0
+while(n<len(var2) and sum1<2):
+    if(var2[n]!=var3[n]):
+        sum1+=1
+    n+=1
+if(sum1==1 or sum1==0):
+    print("yes")
+else:
+    print("no")
