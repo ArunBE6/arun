ac56a4b0a6935a66ae32816222a4c51d6519072d
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-25 11:56:49 +0530

    Update pro_set1_4.py

--- pro_set1_4.py
+++ pro_set1_4.py
@@ -1,33 +1,11 @@
-import sys,string
-def minOps1(A, B):
-    val1 = len(A)
-    num1 = len(B)
-    if num1 != val1:
-        return -1
-    num3 = [0] * 256
-
-    for i in range(num1):
-        num3[ord(B[i])] += 1
-    for i in range(num1):  
-        num3[ord(A[i])] -= 1
-    for i in range(256):
-        if num3[i]:
-            return -1
-    val2 = 0
-    i = num1 - 1
-    j = num1 - 1
-    while i >= 0:
-        while i >= 0 and A[i] != B[j]:
-            i -= 1
-            val2 += 1
-        if i >= 0:
-            i -= 1
-            j -= 1
-    return val2
-
-
-A,B = input().split()
-if A =='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ' and B == 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz' :
-    print(19)
-    sys.exit()
-print(minOps1(A, B))
+num1,num2=map(str,input().split())
+ans1=0
+if len(num1)>len(num2):
+  num1,num2=num2,num1
+val1=0
+while val1<len(num1):
+  ans1+=(ord(num2[val1])-ord(num1[val1]))
+  val1+=1
+for val1 in range(val1,len(num2)):
+  ans1+=ord(num2[val1])-ord('a')+1
+print(ans1)
