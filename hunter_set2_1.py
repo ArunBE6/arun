97355ded632962d0e858309860d5262baa8b96a1
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-25 11:34:57 +0530

    Update hunter_set2_1.py

--- hunter_set2_1.py
+++ hunter_set2_1.py
@@ -1,5 +1,6 @@
-time=input("")
-if time=="Saturtime" or time=="Suntime":
-    print("yes")
-else:
-    print("no")
+import sys, string, math
+line1 = input().split(' ')
+line2 = []
+for s in line1 :
+    line2.append(s[::-1])
+print(*line2)
