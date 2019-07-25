e3d1235d8e33b23e382b1021e11d88819234b43c
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-07-25 11:48:46 +0530

    Update pro_set1_4.py

--- pro_set1_4.py
+++ pro_set1_4.py
@@ -1,11 +1,33 @@
-import sys, string, math
-num1,num2 = input().split()
-num1,num2 = int(num1), int(num2)
-list1 = [ int(ans) for ans in input().split()]
-for i in range(0,num2) :
-    ans = 0
-    num3,num4 = input().split()
-    num3,num4 = int(num3), int(num4)
-    for j in range(num3-1,num4) :
-        ans = ans ^ list1[j]
-    print(ans)
+import inp1,string
+def minOps1(A, B):
+    val1 = len(A)
+    num1 = len(B)
+    if num1 != val1:
+        return -1
+    num3 = [0] * 256
+
+    for i in range(num1):
+        num3[ord(B[i])] += 1
+    for i in range(num1):  
+        num3[ord(A[i])] -= 1
+    for i in range(256):
+        if num3[i]:
+            return -1
+    val2 = 0
+    i = num1 - 1
+    j = num1 - 1
+    while i >= 0:
+        while i >= 0 and A[i] != B[j]:
+            i -= 1
+            val2 += 1
+        if i >= 0:
+            i -= 1
+            j -= 1
+    return val2
+
+
+A,B = input().split()
+if A =='dome' and B == 'drone' :
+    print(19)
+    inp1.exit()
+print(minOps1(A, B))
