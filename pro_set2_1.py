70eda1d96ba3ed673864891bd01e21ccd2231c6d
ArunBE6 <51223058+ArunBE6@users.noreply.github.com>
2019-08-13 09:33:58 +0530

    Update pro_set2_1.py

--- pro_set2_1.py
+++ pro_set2_1.py
@@ -2,10 +2,10 @@ val1,val2=map(int,input().split())
 val3=list(map(int,input().split()))
 val4=[]
 for i in range(0,val2):
-     m,k=map(int,input().split())
-     val4.append([m,k])
+  m,k=map(int,input().split())
+  val4.append([m,k])
 for i in range(val2):
-     low=val4[i][0]
-     up=val4[i][1]
-     ans=sum(val3[low-1:up])
-     print(ans)
+  low=val4[i][0]
+  up=val4[i][1]
+  ans=sum(val3[low-1:up])
+  print(ans)
